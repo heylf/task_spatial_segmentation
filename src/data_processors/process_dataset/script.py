3a3f95eea770e380e76fe3ae2d9e9bb177250a9a
f641l <florian.heyl@dkfz-heidelberg.de>
2026-04-28 11:24:25 +0200

    change dataprocessor to config.vsh.yaml and script.py

--- src/data_processors/process_dataset/script.py
+++ src/data_processors/process_dataset/script.py
@@ -1,9 +1,8 @@
+
 import random
 import anndata as ad
 import spatialdata as sd
-import os
 import scanpy as sc
-import shutil
 
 ## VIASH START
 par = {
@@ -17,6 +16,38 @@ par = {
 }
 ## VIASH END
 
+def sc_processing(adata):
+    if "counts" not in adata.layers and adata.X != None:
+        print(">> Save raw counts in .layer", flush=True)
+        adata.layers["counts"] = adata.X.copy()
+        
+    if "normalized" not in adata.layers:
+        print(">> Perform standard normalization", flush=True)
+        adata.layers["normalized"] = adata.layers["counts"].copy()
+        sc.pp.normalize_total(adata, layer="normalized", inplace=True)
+
+    if "normalized_log" not in adata.layers:
+        print(">> Perform log1p normalization", flush=True)
+        adata.layers["normalized_log"] = adata.layers["normalized"].copy()
+        sc.pp.normalize_total(adata, layer="normalized_log", inplace=True)
+
+    if "normalized_log_scaled" not in adata.layers:
+        print(">> Perform 0 mean and standard variance normalization", flush=True)
+        adata.layers["normalized_log_scaled"] = adata.layers["normalized_log"].copy()
+        sc.pp.normalize_total(adata, layer="normalized_log_scaled", inplace=True)
+
+    if "hvg" not in adata.var:
+        print(">> Compute highly variable genes", flush=True)
+        sc.pp.highly_variable_genes(
+            adata,
+            flavor="seurat_v3",
+            layer="counts",
+            span=par['span'],
+            n_top_genes=par['n_top_genes']
+        )
+        adata.var.rename(columns={"highly_variable": "hvg"}, inplace=True)
+
+
 # set seed if need be
 if par["seed"]:
     print(f">> Setting seed to {par['seed']}")
@@ -24,39 +55,10 @@ if par["seed"]:
 
 print(">> Load data", flush=True)
 sc_data = ad.read_h5ad(par["input_sc"])
+print(f"single cell data: {sc_data}")
 
 print(">> Processing sc_data", flush=True)
-if "counts" not in sc_data.layers and sc_data.X != None:
-    print(">> Save raw counts in .layer", flush=True)
-    sc_data.layers["counts"] = sc_data.X.copy()
-    
-if "normalized" not in sc_data.layers:
-    print(">> Perform standard normalization", flush=True)
-    normalized = sc.pp.normalize_total(sc_data.layers["counts"])
-    sc_data.layers["normalized"] = normalized.copy()
-
-if "normalized_log" not in sc_data.layers:
-    print(">> Perform log1p normalization", flush=True)
-    normalized_log = sc.pp.log1p(sc_data.layers["normalized"])
-    sc_data.layers['normalized_log'] = normalized_log.copy()
-
-if "normalized_log_scaled" not in sc_data.layers:
-    print(">> Perform 0 mean and standard variance normalization", flush=True)
-    normalized_log_scaled = sc.pp.scale(sc_data.layers["normalized"])
-    sc_data.layers['normalized_log_scaled'] = normalized_log_scaled.copy()
-
-if "hvg" not in sc_data.var:
-    print(">> Compute highly variable genes", flush=True)
-    sc.pp.highly_variable_genes(
-        sc_data,
-        flavor="seurat_v3",
-        layer="counts",
-        span=par['span'],
-        n_top_genes=par['n_top_genes']
-    )
-    sc_data.var.rename(columns={"highly_variable": "hvg"}, inplace=True)
-
-print(f"single cell data: {sc_data}")
+sc_processing(sc_data)
 
 print(">> Writing data", flush=True)
 sc_data.write_h5ad(par["output_scrnaseq_reference"], compression="gzip")
@@ -64,11 +66,14 @@ sc_data.write_h5ad(par["output_scrnaseq_reference"], compression="gzip")
 # read input_sp
 print(">> Read spatial data", flush=True)
 sp_data = sd.read_zarr(par["input_sp"])
+print(f"spatial data: {sp_data}")
 
 print(">> Processing spatial data", flush=True)
 sp_data_table = sp_data.tables['table']
+print(f"single cell part of spatial data: {sp_data_table}")
+sc_processing(sp_data_table)
 
-if "cell_area" not in sp_data_table.obs:
+if "ce  ll_area" not in sp_data_table.obs:
     print(">> Perform scanpy qc for cell area", flush=True)
     sc.pp.calculate_qc_metrics(sp_data_table, inplace=True)
 
@@ -80,9 +85,5 @@ for x in ["transcript_counts", "n_genes_by_counts"]:
 print(f"spatial data: {sp_data}")
 
 print(">> Writing spatial data", flush=True)
-# remove directory if it exists
-if os.path.exists(par["output_spatial_dataset"]):
-    shutil.rmtree(par["output_spatial_dataset"])
 sp_data.write(par["output_spatial_dataset"], overwrite=True)
-
 # %%
